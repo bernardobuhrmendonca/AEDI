445fc235e0a6381de3ad428f4c271fd8621035b8
Bernardo Mendonça <bbam2503@gmail.com>
2023-10-18 11:06:27 -0300

    init

--- src/etapa2/D22698B.java
+++ src/etapa2/D22698B.java
@@ -1,15 +1,17 @@
-package etapa2;//Aluno: Bernardo Buhr Alves Mendonça
+package etapa2;
+
+//Aluno: Bernardo Buhr Alves Mendonça
 //Função: determinar quais números de 1000 a 9999 pussuem determinada propriedade, imprimi-los e fazer a média destes
 
 public class D22698B {
     public static void main(String[] args) {
-        int termo, soma=0, cont=0;
+        int termo, soma = 0, cont = 0;
         double quadrado, media;
-        for (int aux=1000; aux<9999; aux++){
+        for (int aux = 1000; aux < 9999; aux++) {
             termo = aux / 100 + aux % 100;
-            quadrado = Math.pow(termo,2.0);
-            if (quadrado==aux){
-                soma+=aux;
+            quadrado = Math.pow(termo, 2.0);
+            if (quadrado == aux) {
+                soma += aux;
                 cont++;
                 System.out.println("O número " + aux + " possui a propriedade.");
             }
