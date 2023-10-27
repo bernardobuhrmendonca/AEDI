5deb667e61c939f894a470a599b833dd1a036779
Berado <bbam2503@gmail.com>
2023-10-27 15:43:15 -0300

    init

--- src/etapa3/C12EX13.java
+++ src/etapa3/C12EX13.java
@@ -1,4 +1,25 @@
 package etapa3;
 
+import java.util.Scanner;
+
 public class C12EX13 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        String nome[] = new String[10];
+        String nomeP;
+        for (int aux = 0; aux<nome.length; aux++) {
+            System.out.println("Indique o nome d a pessoa na posição " + aux + 1);
+            nome[aux] = teclado.nextLine();
+        }
+        System.out.println("Indiqueee um nome a ser pesquisado.");
+        nomeP = teclado.nextLine();
+        for (int aux = 0; aux<nome.length; aux++){
+            if (nomeP.equalsIgnoreCase(nome[aux])){
+                System.out.println("A pessoa pesquisada esta em " + aux + 1 + "o da fila.");
+            }else{
+                System.out.println("Esta pessoa não se encontra na fila.");
+            }
+            teclado.close();
+        }
+    }
 }
---
+++ src/etapa3/C12EX14.java
@@ -0,0 +1,25 @@
+package etapa3;
+
+import java.util.Scanner;
+
+public class C12EX14 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int numeros[] = new int[100];
+        int numeroP;
+        for (int aux = 0; aux<numeros.length; aux++){
+            System.out.println("Indique o número do bilhete " + aux + 1);
+            numeros[aux] = teclado.nextInt();
+        }
+        System.out.println("Indique o número do bilhete premiado.");
+        numeroP = teclado.nextInt();
+        for (int aux = 0; aux<numeros.length; aux++){
+            if (numeros[aux]==numeroP){
+                System.out.println("O bilhete indicado na posição " + aux + 1 + " foi premiado! Parabéns!");
+            }else {
+                System.out.println("Infelizmente nenhum dos bilhetes é o premiado, boa sorte na próxima!");
+            }
+        }
+        teclado.close();
+    }
+}
